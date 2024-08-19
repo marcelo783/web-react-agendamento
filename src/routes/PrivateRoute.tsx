1bdfc5af30df8f305bab66bdaa9ebd3fc947523e
marcelo783 <marcelo.oliveira7931@gmail.com>
2024-08-19 00:14:52 -0300

    redirecionado para register após o login

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import authToken from '@/cookies/appCookies';
-import React from 'react';
+
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,25 +1,31 @@
-
 import { useCookies } from 'react-cookie';
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  const [cookies] = useCookies(['authToken']);
+  const [cookies, setCookie] = useCookies(['authToken']);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!cookies.authToken) {
-      // Redireciona para o login se o token não existir
-      navigate('/login');
-    }///else {
-      console.log('Token encontrado:', cookies.authToken);
-   // }
+    const checkAuthToken = () => {
+      console.log('Tentativa de leitura do authToken:', cookies.authToken);
+
+      if (!cookies.authToken) {
+        navigate('/login');
+      } else {
+        console.log('Token encontrado após nova leitura:', cookies.authToken);
+      }
+    };
+
+    // Espera 200ms e verifica novamente
+    const timeout = setTimeout(checkAuthToken, 200);
+
+    return () => clearTimeout(timeout);
   }, [cookies, navigate]);
 
   return (
     <div>
       <h1>Bem-vindo à página de registro!</h1>
-      {/* Conteúdo da página de registro */}
     </div>
   );
 };
